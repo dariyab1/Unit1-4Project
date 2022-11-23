a4708450f4239c4c09236bb58ab72fe79163df62
dariyab1 <dariyab@nystudents.net>
2022-11-23 09:30:17 -0500

    in the middle of a mental breakdown

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,2 +1,117 @@
-package PACKAGE_NAME;public class Hangman {
+public class Hangman {
+    private String secretWord;
+    private int length;
+    boolean endGame=false;
+    public Hangman(String secretWord){
+        length=secretWord.length();
+        this.secretWord=secretWord;
+    }
+    public boolean letterIsInWord(String letter){
+        if (secretWord.contains(letter)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    public void printMan(int mistakes){
+        if(mistakes==0){
+            System.out.println("  ______");
+            System.out.println(" |      |");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println("---");
+        }
+        if(mistakes==1){
+            System.out.println("  _______");
+            System.out.println(" |       |");
+            System.out.println(" |       _");
+            System.out.println(" |      |_| ");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println("---");
+        }
+        if(mistakes==2){
+            System.out.println("  _______");
+            System.out.println(" |       |");
+            System.out.println(" |       _");
+            System.out.println(" |      |_| ");
+            System.out.println(" |       |");
+            System.out.println(" |       |");
+            System.out.println(" |       |");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println("---");
+
+        }
+        if(mistakes==3){
+            System.out.println("  _______");
+            System.out.println(" |       |");
+            System.out.println(" |       _");
+            System.out.println(" |      |_| ");
+            System.out.println(" |       |");
+            System.out.println(" |      /|");
+            System.out.println(" |     / |");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println("---");
+
+        }
+        if(mistakes==4){
+            System.out.println("  _______");
+            System.out.println(" |       |");
+            System.out.println(" |       _");
+            System.out.println(" |      |_| ");
+            System.out.println(" |       |");
+            System.out.println(" |      /|\\");
+            System.out.println(" |     / | \\");
+            System.out.println(" |       ");
+            System.out.println(" |       ");
+            System.out.println("---");
+
+        }
+        if(mistakes==5){
+            System.out.println("  _______");
+            System.out.println(" |       |");
+            System.out.println(" |       _");
+            System.out.println(" |      |_| ");
+            System.out.println(" |       |");
+            System.out.println(" |      /|\\");
+            System.out.println(" |     / | \\");
+            System.out.println(" |      /");
+            System.out.println(" |     / ");
+            System.out.println("---");
+
+        }
+        if(mistakes==6){
+            System.out.println("  _______");
+            System.out.println(" |       |");
+            System.out.println(" |       _");
+            System.out.println(" |      |_| ");
+            System.out.println(" |       |");
+            System.out.println(" |      /|\\");
+            System.out.println(" |     / | \\");
+            System.out.println(" |      / \\");
+            System.out.println(" |     /   \\");
+            System.out.println("---");
+            System.out.println("You are dead!");
+            endGame=true;
+
+        }
+    }
+
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("User 1 enter a word:");
+        String word=input.nextLine();
+        Hangman newGame=new Hangman(word);
+        for(int i=0; i<100; i++){
+            System.out.println();
+        }
+
+
+        String newStr=word;
+        String playerWord="";
+
+        boolean guessedWord=false;
+        int correctLetters=0;
+        String guess;
+        int correct=0;
+        int mistakes=0;
+        String lettersGuessed="";
+        boolean lost=false;
+
+        while (guessedWord!=true&&lost!=true){
+            int place;
+            System.out.println("User two guess a letter:");
+            guess=input.nextLine();
+            if(mistakes==6){
+                lost=true;
+            }
+
+            if(lettersGuessed.contains(guess)){
+                System.out.println("You already guess that letter, try another one:");
+                guess=input.nextLine();
+            }
+            if (newGame.letterIsInWord(guess)){
+                while(newStr.indexOf(guess)!=-1){
+                    place=newStr.indexOf(guess);
+                    newStr=newStr.substring(0,place)+"0"+newStr.substring(place+1);
+                    correct++;
+
+                }
+            }
+            else if(!(newGame.letterIsInWord(guess))){
+                mistakes++;
+            }
+            for(int i=0; i<100; i++){
+                System.out.println();
+            }
+            newGame.printMan(mistakes);
+            if(correct==word.length()){
+                guessedWord=true;
+            }
+            lettersGuessed+=guess+" ";
+
+        }
+        System.out.println("you guessed the word!");
+        System.out.println(newStr);
+
+
+    }
+
 }
